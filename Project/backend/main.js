ca7c1116584f75cd48ca7e5689de88e94117b015
GhostyJade <giandreas@outlook.it>
2020-03-06 22:24:15 +0100

    Initial commit

--- Project/backend/main.js
+++ Project/backend/main.js
@@ -1,6 +1,11 @@
 const express = require('express')
+const low = require('lowdb')
+const FileSync = require('lowdb/adapters/FileSync')
+
+const adapter = new FileSync('db.json')
+const db = low(adapter)
 const app = express()
 
 const port = 8080
 
-app.listen(port, ()=>{console.log('Server started on port ${port}')})
+app.listen(port, ()=>{console.log(`Example app listening on port ${port}!`)})
