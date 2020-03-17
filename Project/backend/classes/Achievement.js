9497f8dd9ad64982021579fe5f6909ef75cc45b6
GhostyJade <giandreas@outlook.it>
2020-03-17 15:59:17 +0100

    Added class description

---
+++ Project/backend/classes/Achievement.js
@@ -0,0 +1,16 @@
+class Achievement {
+
+    constructor(id, title, data, priority) {
+        this.id = id
+        this.title = title
+        this.data = data
+        this.priority = priority
+    }
+
+    toJson() {
+        return {
+            id, title, data, priority
+        }
+    }
+
+}
