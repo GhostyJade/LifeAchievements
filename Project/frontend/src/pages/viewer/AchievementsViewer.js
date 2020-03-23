f63768fa5d4d7352a00c28ea5d2ab97b58377f1d
GhostyJade <giandreas@outlook.it>
2020-03-23 22:37:06 +0100

    Integrated BoardMaker and LeftSideBar components

--- Project/frontend/src/pages/viewer/AchievementsViewer.js
+++ Project/frontend/src/pages/viewer/AchievementsViewer.js
@@ -1,8 +1,10 @@
 import React from 'react'
 
-import { ListItem, Fab, makeStyles } from '@material-ui/core'
+import { Fab, makeStyles } from '@material-ui/core'
 import { Add as AddIcon } from '@material-ui/icons'
 import AchievementsMaker from './AchievementsMaker'
+import LeftSideBar from './LeftSideBar'
+import BoardMaker from './BoardMaker'
 
 const useStyles = makeStyles(theme => ({
     fabNew: {
@@ -16,18 +18,21 @@ export default function AchievementsViewer() {
 
     const classes = useStyles()
     const [open, setOpen] = React.useState(false)
+    const [boardMakerVisible, setBoardMakerVisible] = React.useState(false)
 
     const onClose = () => {
         setOpen(false)
     }
 
+    const getBoardsList = () => { //TODO
+        console.error("NOT YET IMPLEMENTED")
+    }
+
     return (
         <>
-
+            <LeftSideBar makerAction={setBoardMakerVisible} />
+            {boardMakerVisible ? <BoardMaker /> : null}
             {open ? <AchievementsMaker onClose={onClose} /> : null}
-            <ListItem>
-
-            </ListItem>
             <Fab onClick={setOpen} className={classes.fabNew}>
                 <AddIcon />
             </Fab>
