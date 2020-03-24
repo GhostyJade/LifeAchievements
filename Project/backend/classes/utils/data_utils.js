6107a95efd4efec0df6450e76770cd95e2a0b295
GhostyJade <giandreas@outlook.it>
2020-03-24 20:44:42 +0100

    Added new board method to create a new board

--- Project/backend/classes/utils/data_utils.js
+++ Project/backend/classes/utils/data_utils.js
@@ -1,13 +1,37 @@
 "use strict"
 
+const Achievement = require('./Achievement')
+const Board = require('./Board')
+
 class DataUtils {
     constructor(shortid, dbInstance) {
         this.shortid = shortid
         this.db = dbInstance
     }
 
-    newAchievement = (username, title, data) => {
-        const userId = this.db.get('users').find({username}).value()
+    newBoard = (username, name) => {
+        let boards = this.db.get('users').find({ username }).get('boards').value()
+        if (boards.find(e => { return e.name === name }))
+            return { status: false, result: "board already exists" }; //TODO handle existing board
+        const element = { id: this.shortid.generate(), name }
+        boards.push(element)
+        this.db.get('users').find({ username }).assign({ boards }).write()
+        this.db.get('boards').push({ id: element.id, achievements: [] }).write()
+        return { status: true, result: "board created", data: element }
+    }
+
+    getAllBoards = (username) => {
+        const boardList = this.db.get()
+    }
+
+
+    newAchievement = (username, title, data, boardid = 0) => {
+        const user = this.db.get('users').find({ username }).value()
+        console.log(user)
+        const boardColleciton = this.db.get('boards')
+        console.log(boardColleciton)
+        const achievement = new Achievement(this.shortid.generate(), title, data, null)
+
     }
 }
 
